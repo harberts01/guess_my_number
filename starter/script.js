41eac0d50952884252a72130dc3731fb17549764
Philip Harberts <phil@Philips-MacBook-Pro.local>
2023-03-01 21:42:01 -0600

    Update JS to DRY code

--- starter/script.js
+++ starter/script.js
@@ -9,59 +9,71 @@ let highscore = 0;
 
 document.querySelector('.number').value = number;
 
+const gameMessage = function(message){
+  document.querySelector('.message').textContent = message;
+};
+
+const numText = function(number){
+  document.querySelector('.number').textContent = number;
+};
+
+const bgColor = function(color){
+  document.querySelector('body').style.backgroundColor = color;
+};
+
+const numWidth = function(width){
+  document.querySelector('.number').style.width = width;
+}
+
+const newHiScore = function(x){
+  document.querySelector('.newhighscore').textContent = x;
+}
+
+const liveScore = function(score){
+  document.querySelector('.score').textContent = score;
+}
+
+const hiScoreNum = function (score){
+  document.querySelector('.highscore').textContent =  score;
+}
+
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
-  //When there is no input or
-  if (!guess || guess < 0 || guess > 20) {
-    document.querySelector('.message').textContent = 'Enter a valid Number!';
 
-    //When player guesses the right number
+  
+  if (!guess || guess < 0 || guess > 20) {
+    gameMessage('Enter a valid Number!') ;
   } else if (guess === number) {
-    document.querySelector('.message').textContent = '**** WINNER! ****';
-    document.querySelector('.number').textContent = number; //reveals number in place of the question mark.
-    document.querySelector('body').style.backgroundColor = '#60b347'; //changes background color when game is won.
-    document.querySelector('.number').style.width = '30rem'; //changes width of box around the secret number when game is won.
-    if (score > 0 && score > highscore) {
+    gameMessage('**** WINNER! ****');
+    numText(number);                                        
+    bgColor('#60b347');                                      
+    numWidth('30rem') ;                                      
+    if (score > highscore) {
       highscore = score;
-      document.querySelector('.highscore').textContent = score;
-      document.querySelector('.newhighscore').textContent = 'NEW HIGH SCORE!';
+      hiScoreNum(score);
+      newHiScore('NEW HIGH SCORE!');
     }
     console.log(highscore);
-    //When guess is too high
-  } else if (guess > number) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too High!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'Game Over!';
-      document.querySelector('.score').textContent = 0;
-    }
-
-    //When guess is too low
-  } else if (guess < number) {
-    if (score > 1) {
-      document.querySelector('.message').textContent = 'Too Low!';
-      score--;
-      document.querySelector('.score').textContent = score;
-    } else {
-      document.querySelector('.message').textContent = 'Game Over!';
-      document.querySelector('.score').textContent = 0;
+  }else if (guess !== number){ 
+    if(score > 1){
+      guess > number ? gameMessage('Too High!') : gameMessage('Too Low!');
+      score--; 
+      liveScore(score);
     }
   }
 });
 
-//Again! button function reloads page.
+
 document.querySelector('.again').addEventListener('click', function () {
   score = 20;
   number = Math.trunc(Math.random() * 20 + 1);
   console.log(number);
-  document.querySelector('.score').textContent = score;
-  document.querySelector('.message').textContent = 'Start guessing...';
-  document.querySelector('.number').textContent = '?';
+  liveScore(score);
+  gameMessage('Start guessing...');
+  numText('?');
   document.querySelector('.guess').value = '';
-  document.querySelector('.number').style.width = '15rem';
-  document.querySelector('body').style.backgroundColor = '#222';
-  document.querySelector('.newhighscore').textContent = '';
+  numWidth('15rem');
+  bgColor('#222');
+  newHiScore();
   console.log(highscore);
 });
